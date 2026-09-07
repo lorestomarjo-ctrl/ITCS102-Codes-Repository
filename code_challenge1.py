0fc33e6830b070aadfc01102c9a8b74e2c9d094f
Marjorie Loresto <lorestomarjo@gmail.com>
2026-09-07 16:02:00 +0800

    Delete code_challenge1.py

--- code_challenge1.py
+++
@@ -1,4 +0,0 @@
-name= '\t\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t' '\n\n\n\n' '\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*' '\n\n\n\n' '\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*' '\n\n\n\n' '\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t*' '\n\n\n\n' '\t\t\t\t*\t\t\t\t\t\tHi, my name is Janna Mae Satoya\t\t\t\t\t\t\t*' '\n\n\n\n' '\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t*' '\n\n\n\n' '\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*'  '\n\n\n\n' '\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*' '\n\n\n\n' '\t\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t'	
-
-
-print(name)	
